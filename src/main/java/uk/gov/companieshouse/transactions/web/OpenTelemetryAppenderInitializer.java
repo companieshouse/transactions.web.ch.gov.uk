d6f3bf8d147586e2f82cfa297de132c9fec656b2
garethpowell-ch <gpowell1@companieshouse.gov.uk>
2026-07-15 15:32:16 +0100

    ASM-2641 Update to Spring Boot 4.1 and patch dependencies.

--- src/main/java/uk/gov/companieshouse/transactions/web/OpenTelemetryAppenderInitializer.java
+++ src/main/java/uk/gov/companieshouse/transactions/web/OpenTelemetryAppenderInitializer.java
@@ -4,9 +4,11 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(prefix = "management.opentelemetry", name = "enabled", havingValue = "true")
 class OpenTelemetryAppenderInitializer implements InitializingBean {
 
     private final OpenTelemetry openTelemetry;
