625b82a94db1465126ccc0a1f702e198f2b5518e
LMarshallAfzalCH <224774279+LMarshallAfzalCH@users.noreply.github.com>
2026-09-25 09:33:29 +0100

    set default health check grace period to 180 seconds

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -39,6 +39,7 @@ module "ecs-service" {
   use_task_container_healthcheck = true
   healthcheck_path                  = local.healthcheck_path
   healthcheck_matcher               = local.healthcheck_matcher
+  health_check_grace_period_seconds = var.health_check_grace_period_seconds
 
   # Docker container details
   docker_registry   = var.docker_registry
